4046ba8e35a6674198ccdf64351784e33e83a010
Art3mis20726 <gitpats@gmail.com>
2024-08-26 15:05:33 +0530

    "Added login and logout functionality to user controller and routes"

--- Backend/src/controllers/user.controllers.js
+++ Backend/src/controllers/user.controllers.js
@@ -2,7 +2,17 @@ import { User } from "../models/user.models.js";
 import ApiError from "../utils/apiError.utils.js";
 import { ApiResponse } from "../utils/apiResponse.utils.js";
 import asyncHandler from "../utils/asyncHandler.utils.js";
-
+const generateToken = async (userId)=>{
+    const user = await User.findById(userId);
+    if(!user) {
+        throw new ApiError(400,"User not Found")
+    }
+    const accessToken=await user.generateAccessToken();
+    const refreshAccessToken = user.generateRefreshAccessToken();
+    user.refreshAccessToken = refreshAccessToken;
+    await user.save({ validateBeforeSave: false }); //Saving it in the user
+    return { accessToken, refreshAccessToken };
+}
 const registerUser=asyncHandler(async(req,res)=>{
 const{userName,email,password,fullName,address,age,gender}=req.body
 if(!userName){
@@ -48,6 +58,62 @@ if(!newlycreatedUser){
 return res.status(200).json(new ApiResponse(200,newlycreatedUser,"User Created Successfully"))
 
 })
+const loginUser=asyncHandler(async(req,res)=>{
+    const {userName,password}=req.body
+    if(!userName){
+        throw new ApiError(400,"UserName not provided")
+    }
+    if(!password){
+        throw new ApiError(400,"Password not provided")
+    }
+    const user=await User.findOne({userName})
+    if(!user){
+        throw new ApiError(400,"The user is not registered")
+    }
+    const isPasswordCorrect=await user.isPasswordCorrect(password)
+    if(!isPasswordCorrect){
+        throw new ApiError(400,"Password is incorrect")
+    }
+    const { accessToken, refreshAccessToken }=await generateToken(user._id)
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    );
+    const Options = {
+        httpOnly: false,
+        secure: true,
+        sameSite:'None',
+    };
+    return res
+            .status(200)
+            .cookie("accessToken",accessToken,Options)
+            .cookie("refreshAccessToken",refreshAccessToken,Options)
+            .json(new ApiResponse(200, loggedInUser,"User logged in successfully"))
+
+
+
+})
+const logoutUser=asyncHandler(async(req,res)=>{
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+          $unset: { refreshToken: 1 },
+        },
+        { new: true }
+      );
+    
+      const Options = {
+        httpOnly: false,
+        secure: true,
+        sameSite: 'None',
+        path: '/', // Ensure the path is set correctly
+      };
+    
+      return res
+        .status(200)
+        .clearCookie("accessToken", Options)
+        .clearCookie("refreshAccessToken", Options)
+        .json(new ApiResponse(200, "User logged out successfully!!"));
+    });
 export{
-    registerUser
+    registerUser,loginUser,logoutUser
 }
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router=Router()
 router.route("/registerUser").post(registerUser)
+router.route("/loginUser").post(loginUser)
+router.route("/logoutUser").get(verifyJWT,logoutUser)
 export default router
